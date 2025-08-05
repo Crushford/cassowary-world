5c57e7f3220b62c62de2e01dcf70a5bdf3d2b975
James Rushford <jrushford@gitlab.com>
2025-08-05 22:25:17 +0200

    Add footer and fix fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,16 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: 'var(--font-heading)',
+        body: 'var(--font-body)',
+        mono: 'var(--font-mono)'
+      }
+    }
+  }
 }
 
 export default config
